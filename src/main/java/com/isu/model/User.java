2373c9778f96e6049035a8c7730d2cd0f6e98d73
Arjun <mail.arjun.bh@gmail.com>
2017-09-04 04:16:57 -0500

    First Commit

---
+++ src/main/java/com/isu/model/User.java
@@ -0,0 +1,48 @@
+package com.isu.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	private String userName;
+	@Column(name = "firstname", nullable = false)
+	private String firstName;
+	@Column(name = "lastname")
+	private String lastName;
+	@Column(name = "role")
+	private String role = "TA";		//The default role is TA
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+}
